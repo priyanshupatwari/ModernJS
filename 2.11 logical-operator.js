379403bcf0c23f971e71d8fd10b203433970d09b
Priyanshu Patwari <torrentu96@gmail.com>
2022-04-26 20:28:44 +0530

    first commit

---
+++ 2.11 logical-operator.js
@@ -0,0 +1,8 @@
+// Write an if condition to check that age is between 14 and 90 inclusively.
+// “Inclusively” means that age can reach the edges 14 or 90.
+let age = 18;
+if (age >= 14 && age <= 90) {
+  console.log("age is between 14 and 90");
+} else {
+  console.log(" age is not  between 14 and 90");
+}
